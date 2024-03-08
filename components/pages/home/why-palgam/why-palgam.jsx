1b2cc87c7df45fb4749feb15f10844b4914053a3
alivolley <alicryptovolley@gmail.com>
2024-03-08 13:22:54 +0330

    home , why palgam : first and second section ui and animation finished and responsived

--- components/pages/home/why-palgam/why-palgam.jsx
+++ components/pages/home/why-palgam/why-palgam.jsx
@@ -2,6 +2,10 @@ import Image from 'next/image';
 
 // Assets
 import WhyPalgamStyle from './why-palgam.style';
+import profileFrame from '@/assets/images/whyPalgamProfileFrame.png';
+import dotedNoise from '@/assets/images/dotedNoise.png';
+import whyPalgamPurpleLines from '@/assets/images/whyPalgamPurpleLines.png';
+import whyPalgamTrophy from '@/assets/images/whyPalgamTrophy.png';
 
 const importAll = r => r.keys().map(r);
 const images = importAll(require.context('@/assets/images/cards', false, /\.(png|jpe?g|svg)$/));
@@ -21,9 +25,12 @@ function WhyPalgam() {
             environment filled with outstanding features for a gaming experience like no other.
          </p>
          <div className="mt-[56px]">
-            <div className="flex gap-6">
-               <div className="relative h-[360px] flex-1 shrink-0 overflow-hidden rounded-[24px]" id="firstWrapper">
-                  <div className="absolute left-1/2 top-1/2 mt-[-30px] -translate-x-1/2 -translate-y-1/2">
+            <div className="flex flex-col gap-6 customMd:flex-row">
+               <div
+                  className="relative h-[360px] shrink-0 overflow-hidden rounded-[24px] customMd:flex-1"
+                  id="firstWrapper"
+               >
+                  <div className="absolute left-1/2 top-1/2 mt-[-40px] -translate-x-1/2 -translate-y-1/2">
                      <div className="flex items-center gap-2 transition-all duration-300" id="firstWrapperRow1">
                         {firstArray?.map(item => (
                            <div
@@ -72,11 +79,83 @@ function WhyPalgam() {
                   </div>
 
                   <div className="absolute inset-0 bg-[#D3D3D3] opacity-[0.1]" />
+
+                  <div className="relative flex h-full flex-col justify-end p-6 text-white">
+                     <p className="font-poppinsExtraBold text-[20px] leading-[28px] customMd:text-[24px] customMd:leading-[32px]">
+                        Exceptional Gameplay and Graphics
+                     </p>
+                     <p className="mt-1 font-poppinsThin text-sm leading-[24px]">
+                        Our games are designed to be visually stunning and engaging, offering a seamless and captivating
+                        experience on mobile devices.
+                     </p>
+                  </div>
                </div>
-               <div className="flex-1 shrink-0 text-white">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus officia, perspiciatis veniam
-                  necessitatibus totam veritatis fugit placeat mollitia magnam earum ipsam at, iure accusamus facere
-                  suscipit consequatur animi harum labore.
+               <div
+                  className="relative flex shrink-0 flex-col justify-between overflow-hidden rounded-[24px] bg-[#8c72e21a] customMd:h-[360px] customMd:flex-1"
+                  id="trophyWrapper"
+               >
+                  <div className="mt-[48px] flex items-center justify-center gap-3">
+                     <div className="size-[120px] shrink-0">
+                        <Image src={profileFrame} alt="profile frame" className="size-full" />
+                     </div>
+                     <div className="size-[120px] shrink-0">
+                        <Image src={profileFrame} alt="profile frame" className="size-full" />
+                     </div>
+                     <div className="size-[120px] shrink-0">
+                        <Image src={profileFrame} alt="profile frame" className="size-full" />
+                     </div>
+                     <div className="size-[120px] shrink-0">
+                        <Image src={profileFrame} alt="profile frame" className="size-full" />
+                     </div>
+                     <div
+                        className="h-[160px] w-[220px] shrink-0 rounded-[24px] bg-[#8c72e280] p-2 transition-all duration-1000"
+                        style={{ boxShadow: '0px 12px 64px 0px rgba(0, 0, 0, 0.20)' }}
+                        id="mainTrophyBox"
+                     >
+                        <div className="relative">
+                           <div className="h-[104px] w-full overflow-hidden rounded-[18px]">
+                              <div
+                                 className="size-full overflow-hidden rounded-[18px] transition-all duration-1000"
+                                 id="mainTrophyBoxImage"
+                              >
+                                 <Image src={whyPalgamPurpleLines} alt="lines" className="size-full" />
+                              </div>
+                           </div>
+                           <div className="absolute left-1/2 top-1/2 z-[3] -translate-x-1/2 -translate-y-1/2">
+                              <Image src={whyPalgamTrophy} alt="trophy" />
+                           </div>
+                           <div
+                              id="trophyBackGround"
+                              className="absolute left-1/2 top-1/2 z-[2] -translate-x-1/2 -translate-y-1/2"
+                           />
+                        </div>
+                        <div className="mx-auto mt-4 h-5 w-[120px] rounded-[64px] bg-[#8c72e266]" />
+                     </div>
+                     <div className="size-[120px] shrink-0">
+                        <Image src={profileFrame} alt="profile frame" className="size-full" />
+                     </div>
+                     <div className="size-[120px] shrink-0">
+                        <Image src={profileFrame} alt="profile frame" className="size-full" />
+                     </div>
+                     <div className="size-[120px] shrink-0">
+                        <Image src={profileFrame} alt="profile frame" className="size-full" />
+                     </div>
+                     <div className="size-[120px] shrink-0">
+                        <Image src={profileFrame} alt="profile frame" className="size-full" />
+                     </div>
+                  </div>
+                  <div className="relative p-6 text-white">
+                     <p className="font-poppinsExtraBold text-[20px] leading-[28px] customMd:text-[24px] customMd:leading-[32px]">
+                        Exciting Tournaments
+                     </p>
+                     <p className="mt-1 font-poppinsThin text-sm leading-[24px]">
+                        Palgam hosts thrilling tournaments, providing users with competitive gaming opportunities and
+                        chances to win exciting prizes.
+                     </p>
+                  </div>
+                  <div className="absolute inset-0">
+                     <Image src={dotedNoise} alt="profile frame" className="size-full" />
+                  </div>
                </div>
             </div>
          </div>
--- components/pages/home/why-palgam/why-palgam.style.js
+++ components/pages/home/why-palgam/why-palgam.style.js
@@ -27,6 +27,35 @@ const WhyPalgamStyle = styled.div(() => ({
          },
       },
    },
+
+   '#trophyBackGround': {
+      transition: 'all 0.7s',
+      width: '198px',
+      height: '102px',
+      backgroundColor: '#E1D8FF',
+      opacity: '0.2',
+      filter: 'blur(12px)',
+   },
+
+   '#trophyWrapper': {
+      ':hover': {
+         '#mainTrophyBox': {
+            transform: 'scale(1.2)',
+            margin: '0 22px',
+         },
+         '#mainTrophyBoxImage': {
+            transform: 'rotate(45deg) scale(2.5)',
+         },
+         '#trophyBackGround': {
+            width: '260px',
+            height: '134px',
+            backgroundColor: '#E1D8FF',
+            opacity: '0.2',
+            filter: 'blur(60px)',
+            borderRadius: '259px',
+         },
+      },
+   },
 }));
 
 export default WhyPalgamStyle;
